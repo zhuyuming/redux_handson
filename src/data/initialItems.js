af4f2734dbc8a2dcb0226cc781a88ac84327fd5d
kazuaki.okamoto <kazuaki.okamoto@gmail.com>
2016-04-04 18:43:43 +0900

    srcへも初期データを記述する

---
+++ src/data/initialItems.js
@@ -0,0 +1,34 @@
+const productItems = [
+    {
+        id    : 1,
+        name  : "速習ECMAScript6: 次世代の標準JavaScriptを今すぐマスター！",
+        price : 1500,
+        cart  : false
+    },
+    {
+        id    : 2,
+        name  : "確かな力が身につくJavaScript「超」入門 (Informatics & IDEA)",
+        price : 800,
+        cart  : false
+    },
+    {
+        id    : 3,
+        name  : "開眼! JavaScript ―言語仕様から学ぶJavaScriptの本質",
+        price : 2300,
+        cart  : false
+    },
+    {
+        id    : 4,
+        name  : "確かな力が身につくJavaScript「超」入門 (確かな力が身につく「超」入門シリーズ)",
+        price : 3200,
+        cart  : false
+    },
+    {
+        id    : 5,
+        name  : "ノンプログラマのためのJavaScriptはじめの一歩 (WEB+DB PRESS plus)",
+        price : 1600,
+        cart  : false
+    }
+];
+
+export default productItems;
